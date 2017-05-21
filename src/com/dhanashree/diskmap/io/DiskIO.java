da7f78df4baed8d94dfcbe1c19c5a3137d48e1f4
unknown <dhanashree.mahadik.official@gmail.com>
2017-05-21 14:04:33 +0530

    Disk Backed Map

---
+++ src/com/dhanashree/diskmap/io/DiskIO.java
@@ -0,0 +1,28 @@
+package com.dhanashree.diskmap.io;
+
+import com.dhanashree.diskmap.Record;
+
+public interface DiskIO extends Iterable<Record> {
+
+    Record lookup(long location);
+
+    long write(Record r);
+
+    void update(Record r);
+
+    void update(Record...rs);
+
+    long size();
+
+    void close();
+
+    void vacuum(RecordFilter filter) throws Exception;
+
+    void clear();
+
+    public interface RecordFilter{
+        public boolean accept(Record r);
+
+        void update(Record r, long newLocation);
+    }
+}
